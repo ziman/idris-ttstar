523cc11d776d415a146b2c65ad3ee877f1a61c6a
Matus Tejiscak <ziman@functor.sk>
2017-03-27 15:19:30 +0100

    Initial commit.

---
+++ src/Main.hs
@@ -0,0 +1,45 @@
+module Main where
+
+import Idris.Core.TT
+import Idris.AbsSyntax
+import Idris.ElabDecls
+import Idris.REPL
+import Idris.Main
+
+import IRTS.Compiler
+import IRTS.CodegenTTstar
+
+import System.Environment
+import System.Exit
+
+import Paths_idris_python
+
+data Opts = Opts { inputs :: [FilePath],
+                   output :: FilePath }
+
+showUsage = do putStrLn "Usage: idris-ttstar <ibc-files> [-o <output-file>]"
+               exitWith ExitSuccess
+
+getOpts :: IO Opts
+getOpts = do xs <- getArgs
+             return $ process (Opts [] "a.tt") xs
+  where
+    process opts ("-o":o:xs) = process (opts { output = o }) xs
+    process opts ("--yes-really":xs) = process opts xs -- TODO
+    process opts (x:xs) = process (opts { inputs = x:inputs opts }) xs
+    process opts [] = opts
+
+c_main :: Opts -> Idris ()
+c_main opts = do elabPrims
+                 loadInputs (inputs opts) Nothing
+                 mainProg <- elabMain
+                 ir <- compile (Via IBCFormat "ttstar") (output opts) (Just mainProg)
+                 runIO $ codegenPython ir
+
+main :: IO ()
+main = do opts <- getOpts
+          if (null (inputs opts)) 
+             then showUsage
+             else runMain (c_main opts)
+
+
